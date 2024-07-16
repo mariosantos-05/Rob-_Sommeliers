e27b23a2907c9fbbebe0192b9dfd53e96ee1fba2
mariosantos-05 <marioaugustov.s@gmail.com>
2024-07-16 15:23:03 -0300

    Feat:changes to Main, setup and minor changes

--- Movement/include/ColorSensor.h
+++ Movement/include/ColorSensor.h
@@ -5,18 +5,16 @@
 
 class ColorSensor {
 private:
-    uint8_t redPin;
-    uint8_t greenPin;
-    uint8_t bluePin;
     uint8_t s0Pin;
     uint8_t s1Pin;
     uint8_t s2Pin;
     uint8_t s3Pin;
+    uint8_t outPin;
     
     void setFrequency(uint8_t freq);
 
 public:
-    ColorSensor(uint8_t s0, uint8_t s1, uint8_t s2, uint8_t s3, uint8_t red, uint8_t green, uint8_t blue);
+    ColorSensor(uint8_t s0, uint8_t s1, uint8_t s2, uint8_t s3, uint8_t out);
     void begin();
     void readColor(int *red, int *green, int *blue);
 };
--- Movement/src/main.cpp
+++ Movement/src/main.cpp
@@ -19,26 +19,40 @@ const int sensorLeftEcho = 10;
 const int sensorRightTrig = 11;
 const int sensorRightEcho = 12;
 const int sensorFrontTrig = 13;
+const int encoderA = 15;
+const int encoderB = 16;
 const int sensorFrontEcho = A0;
 const int redLEDPin = A1;
 const int greenLEDPin = A2;
-const int blueLEDPin = A3; 
+const int blueLEDPin = A3;
+const int S0 = 7;
+const int S1 = 6;
+const int S2 = 5;
+const int S3 = 4;
+const int Coutput = 3;
 
 // Criando os objetos referente as partes
-MotorDC motorLeft(motorLeftPin1, motorLeftPin2, motorLeftEn);
-MotorDC motorRight(motorRightPin1, motorRightPin2, motorRightEn);
+MotorDC motorLeft(motorLeftPin1, motorLeftPin2, motorLeftEn, encoderA, encoderB );
+MotorDC motorRight(motorRightPin1, motorRightPin2, motorRightEn, encoderA, encoderB);
 Sensor sensorLeft(sensorLeftTrig, sensorLeftEcho);
 Sensor sensorRight(sensorRightTrig, sensorRightEcho);
 Sensor sensorFront(sensorFrontTrig, sensorFrontEcho);
-Servo claw;
-ColorSensor colorSensor(0, 1, 2, 3, 2, 3, 4); 
+Servo claw, stacker;
+ColorSensor colorSensor(S0, S1, S2, S3, Coutput); 
 LED led(redLEDPin, greenLEDPin, blueLEDPin);
 
 // Construtor do robo
-Robot robot(motorLeft, motorRight, claw, sensorLeft, sensorRight, sensorFront, colorSensor, led);
+Robot robot(motorLeft, motorRight, claw, stacker , sensorLeft, sensorRight, sensorFront, colorSensor, led);
 
 void setup() {
+    Serial.begin(9600);
+    colorSensor.begin();
     claw.attach(clawPin);
+    pinMode(redLEDPin, OUTPUT);
+    pinMode(greenLEDPin, OUTPUT);
+    pinMode(blueLEDPin, OUTPUT);
+    pinMode(sensorLeftTrig, OUTPUT);
+    pinMode(sensorLeftEcho, INPUT);
     // Inicializando componentes se necessario
 }
 
@@ -56,8 +70,44 @@ int closedPosition = 90;
 // Estrategia principal de navegação do robô (quase um pseudodigo por enquanto!)
 
 void loop() {
+
+
+    /*
+    robot.SetLEDColor(0, 255, 0);
+    robot.Close_Claw(closedPosition);
+    delay(1000); 
+
+    robot.Open_Claw(openPosition);
+    delay(1000);*/
+
+   // Serial.println("Green");
+
+    /*robot.Close_Claw(closedPosition);
+    delay(1000); 
+
+    robot.Open_Claw(openPosition);
+    delay(1000);
+
+    robot.SetLEDColor(255, 0, 0);
+   /// Serial.println("Red");
+    delay(1000);
+
+    // Set color to Green
+    robot.SetLEDColor(0, 255, 0);
+   // Serial.println("Green");
+    delay(1000);
+
+    robot.SetLEDColor(0, 0, 255);
+    // Set color to Blue
+   // Serial.println("Blue");
+    delay(1000);*/
+
+    //Serial.println(robot.Get_L_Distance());
+
+
+
     // Main robot loop
-    /*char receivedLetter = robot.Vision();
+    char receivedLetter = robot.Vision();
     switch (receivedLetter) {
     case 'A':
         break;
@@ -68,9 +118,9 @@ void loop() {
             robot.Align(tolerance_Align);
             robot.Straight_Run();
         }
-        robot.Turn_Left();
+        robot.Turn_Left_90();
         robot.Straight_Run_cm(110); // Aqui ele já deve estar dentro do caixa 1 / este valor deve ser testado
-        robot.Turn_Left();
+        robot.Turn_Left_90();
         int cubos = 0;
         while (cubos > 7){
             // pensar melhor sobre como pegar os cubos 
@@ -78,30 +128,30 @@ void loop() {
             while (cubo not in sight){
                 robot.Straight_Run();
             }
-            claw.writeMicroseconds(openPosition);
+            robot.Open_Claw(openPosition);
             robot.Straight_Run_cm(5);
-            claw.writeMicroseconds(closedPosition);
+            robot.Close_Claw(closedPosition);
             robot.Update_LED_CS(); //liga o led da cor do cubo
-            robot.Straight_Back(10); //TODO: FAZER UM METODO RUN BACK EM CM
-            robot.Turn_right();
+            robot.Straight_Back_cm(10); 
+            robot.Turn_right_90();
             MOVE_UNTIL_DISTANCE(robot, tolerance_front);
-            robot.Turn_right();
+            robot.Turn_right_90();
             MOVE_UNTIL_DISTANCE(robot, tolerance_front);
-            robot.Turn_right();
+            robot.Turn_right_90();
             robot.Straight_Run_cm(150/2);
-            robot.Turn_Left();
+            robot.Turn_Left_90();
             // Entrega o cubo
             cubos++;
             MOVE_UNTIL_DISTANCE(robot, tolerance_front);
-            robot.Turn_Left();
+            robot.Turn_Left_90();
         }
 
         MOVE_UNTIL_DISTANCE(robot, tolerance_front);
-        robot.Turn_right();
+        robot.Turn_right_90();
         robot.Straight_Run_cm(150/2);
-        robot.Turn_Left();
+        robot.Turn_Left_90();
         MOVE_UNTIL_DISTANCE(robot, tolerance_front);
-        robot.Turn_Left();
+        robot.Turn_Left_90();
         robot.Straight_Run_cm(150*2); //vai parar na porta do caso B
         break;
 
@@ -111,9 +161,9 @@ void loop() {
             robot.Align(tolerance_Align);
             robot.Straight_Run();
         }
-        robot.Turn_right();
+        robot.Turn_right_90();
         robot.Straight_Run_cm(110); // Aqui ele já deve estar dentro do caixa 1 / este valor deve ser testado
-        robot.Turn_right();
+        robot.Turn_right_90();
         int cubos = 0;
         while (cubos > 7){
             // pensar melhor sobre como pegar os cubos 
@@ -125,31 +175,31 @@ void loop() {
             robot.Straight_Run_cm(5);
             claw.writeMicroseconds(closedPosition);
             robot.Update_LED_CS(); //liga o led da cor do cubo
-            robot.Straight_Back(10); //TODO: FAZER UM METODO RUN BACK EM CM
-            robot.Turn_Left();
+            robot.Straight_Back_cm(10); 
+            robot.Turn_Left_90();
             MOVE_UNTIL_DISTANCE(robot, tolerance_front);
-            robot.Turn_Left();
+            robot.Turn_Left_90();
             MOVE_UNTIL_DISTANCE(robot, tolerance_front);
-            robot.Turn_Left();
+            robot.Turn_Left_90();
             robot.Straight_Run_cm(150/2);
-            robot.Turn_right();
+            robot.Turn_right_90();
             // Entrega o cubo
             cubos++;
             MOVE_UNTIL_DISTANCE(robot, tolerance_front);
-            robot.Turn_right();
+            robot.Turn_right_90();
         }
 
         MOVE_UNTIL_DISTANCE(robot, tolerance_front);
-        robot.Turn_Left();
+        robot.Turn_Left_90();
         robot.Straight_Run_cm(150/2);
-        robot.Turn_right();
+        robot.Turn_right_90();
         MOVE_UNTIL_DISTANCE(robot, tolerance_front);
-        robot.Turn_right();
+        robot.Turn_right_90();
         robot.Straight_Run_cm(150*2); //vai parar na porta do caso A
 
         break;
     default:
         //stub
         break;
-    }*/
+    }
 }   
